05eb392c42a9dd0945cc98581d7c7bb942229f9f
Kiarabel <Kiarabelamide@yahoo.com.au>
2019-05-25 11:30:12 +0800

    Shanthi theme duplicate

---
+++ footer.php
@@ -0,0 +1,22 @@
+
+
+    <div class="container-fluid footerbg">
+        <footer class="container">
+            <div class="row">
+                <div class="col-xs-6">
+                    <p> Copyright 2019</p>
+                </div>
+
+
+                <div class="col-xs-6 terms">
+                    <p> Terms Conditions</p>
+                </div>
+
+            </div>
+        </footer>
+    </div>
+
+</body>
+
+</html>
+<?php wp_footer(); ?>
