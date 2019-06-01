281570252c00d739d40f350b4125e2812c283006
Kiarabel <Kiarabelamide@yahoo.com.au>
2019-06-01 13:21:09 +0800

    New

--- page_template_home.php
+++ page_template_home.php
@@ -6,23 +6,31 @@
     ?>
 <?php get_header(); /* Tells WordPress to include header.php */ ?>
 
+
+
 <div class="container-fluid cardsselection">
         <div class="container">
             <div class="row">
 
                 <div class="col-sm-4">
-                   <?php the_field('cards_image'); ?>
-                     <?php the_field('cardstext'); ?>
+                   <?php the_field('card_image_1'); ?>
+                     <?php the_field('cards'); ?>
 
                 <div class="col-sm-4">
-                    <img src=http://localhost:8080/shanthi-wordpress/wp-content/themes/shanthi-theme/images/Valentine.jpg> <h3>Valentine's</h3>
-                    <p class=cardstext>Give your loved one a card made with lots of love. Hearts in any colours available in the colour wheel!</p>
+                  <?php the_field('card_image_2'); ?>
+                     <?php the_field('cards'); ?>
                 </div>
 
                 <div class="col-sm-4">
-                    <img src=http://localhost:8080/shanthi-wordpress/wp-content/themes/shanthi-theme/images/Mothersday.jpg> <h3>Mother's Day</h3>
-                    <p class="cardstext">A day for the most beautiful woman on Earth! Treat her with amazing visuals from our range of cards.</p>
+                    <?php the_field('card_image_3'); ?>
+                     <?php the_field('cards'); ?>
+                 
                 </div>
+                    
+                </div>
+            </div>
+            </div>
+            </div>
 
 
 <?php get_footer(); /* Tells WordPress to include footer.php */ ?>
